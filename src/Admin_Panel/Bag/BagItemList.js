208a9fcfc4ac3aedf0f153081270fcc0e44fce7f
NandhiniSiva37 <nandhinisiva1237@gmail.com>
2021-06-14 15:48:52 +0530

    fixing

--- src/Admin_Panel/Bag/BagItemList.js
+++ src/Admin_Panel/Bag/BagItemList.js
@@ -12,21 +12,11 @@ class BagItemList extends React.Component {
               description={item.menu_quantity_measure_price.menu.short_descr}
               itemName={item.menu_quantity_measure_price.menu.name}
               image={item.menu_quantity_measure_price.menu.image}
-<<<<<<< HEAD
-
-              price={this.props.price_details[index].order_detail.original_price}
-              
+              price={this.props.price_details[index].order_detail.original_price}              
               discountPrice={this.props.price_details[index].order_detail.price}
-              
-=======
-              // price={this.props.price_details[index].order_detail.original_price}
-              price={item.order_detail.original_price}
-              // discountPrice={this.props.price_details[index].order_detail.price}
-              discountPrice={item.order_detail.price}
->>>>>>> origin/release
+              price={this.props.price_details[index].order_detail.original_price}
               discount={item.menu_quantity_measure_price.menu.discount}
-              // quantity={this.props.price_details[index].order_detail.quantity}
-              quantity={item.order_detail.quantity}
+              quantity={this.props.price_details[index].order_detail.quantity}
               measure_values={item.menu_quantity_measure_price.measure_values.name}
               quantity_values={item.menu_quantity_measure_price.quantity_values.quantity}
              
