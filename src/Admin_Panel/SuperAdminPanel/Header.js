340d007cdad559465f259891e060bf2f2efdde12
harith-beckham <harith.kv47@gmail.com>
2021-06-15 13:53:03 +0530

    Fix:Dropdown options all are present in their respective panels

--- src/Admin_Panel/SuperAdminPanel/Header.js
+++ src/Admin_Panel/SuperAdminPanel/Header.js
@@ -5,6 +5,8 @@ import { roles_and_IDs } from "../../helpers/constants";
 import Dropdown from "react-bootstrap/Dropdown";
 
 function Header(props) {
+  const [show, setShow] = React.useState(false);
+  
   return (
     <>
       {!props.isMobile ? (
@@ -15,53 +17,89 @@ function Header(props) {
               <label className="ml-20 mb-0">{props.name}</label>
             </div>
             <div className="col-auto ml-auto d-flex align-items-center">
-              <MenuDropDown employeeName={props.employeeName} />
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-custom-components">
+                  <div className="user-btn" onclick={() => setShow(!show)}>
+                    <i class="lni lni-user user-icon"></i>
+                  </div>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu show={show}>
+                  <Dropdown.Item>Welcome, {props.employeeName} </Dropdown.Item>
+                  <Dropdown.Item>
+                    {props.roleID === roles_and_IDs["Super Admin"] && (
+                      <Link to="/createOfflineOrders">
+                        Create Offline Orders
+                </Link>
+                    )}
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    {props.roleID === roles_and_IDs["Smart Diner Super Admin"] && (
+                      <Link to="/create-restaurant">
+                        Create New Restaurant
+                      </Link>
+                    )}
+                  </Dropdown.Item>
+
+                  <Dropdown.Item
+                    onClick={() => {
+                      localStorage.removeItem("token");
+                    }}
+                  >
+                    <Link to="/login">Logout</Link>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
             </div>
           </div>
         </div>
       ) : (
-        <div className="adminPanel-mob-header">
-          <div className="toggle_icon" onClick={props.setOpenSideMenu}>
-            <span></span>
+          <div className="adminPanel-mob-header">
+            <div className="toggle_icon" onClick={props.setOpenSideMenu}>
+              <span></span>
+            </div>
+            <h6 className="mt-10 mb-0">{props.name}</h6>
+            <div className="d-flex align-items-center">
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-custom-components">
+                  <div className="user-btn" onclick={() => setShow(!show)}>
+                    <i class="lni lni-user user-icon"></i>
+                  </div>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu show={show}>
+                  <Dropdown.Item>Welcome,{props.employeeName} </Dropdown.Item>
+                  <Dropdown.Item>
+                    {props.roleID === roles_and_IDs["Super Admin"] && (
+                      <Link to="/createOfflineOrders">
+                        Create Offline Orders
+                </Link>
+                    )}
+                  </Dropdown.Item>
+                  <Dropdown.Item>
+                    {props.roleID === roles_and_IDs["Smart Diner Super Admin"] && (
+                      <Link to="/create-restaurant">
+                        Create New Restaurant
+                      </Link>
+                    )}
+                  </Dropdown.Item>
+
+                  <Dropdown.Item
+                    onClick={() => {
+                      localStorage.removeItem("token");
+                    }}
+                  >
+                    <Link to="/login">Logout</Link>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+            </div>
           </div>
-          <h6 className="mt-10 mb-0">{props.name}</h6>
-          <div className="d-flex align-items-center">
-            <MenuDropDown employeeName={props.employeeName} />
-          </div>
-        </div>
-      )}
+        )}
     </>
   );
 }
 
 export default Header;
-
-const MenuDropDown = ({ employeeName }) => {
-  const [show, setShow] = React.useState(false);
-  return (
-    <Dropdown>
-      <Dropdown.Toggle id="dropdown-custom-components">
-        <div className="user-btn" onclick={() => setShow(!show)}>
-          <i class="lni lni-user user-icon"></i>
-        </div>
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu show={show}>
-        <Dropdown.Item>Welcome, {employeeName}</Dropdown.Item>
-        <Dropdown.Item>
-          <Link to="/create-restaurant">Create New Restaurant</Link>
-        </Dropdown.Item>
-        {/* <Dropdown.Item>
-          <Link to="/admin-panel/edit-menu">Edit Menu</Link>
-        </Dropdown.Item> */}
-        <Dropdown.Item
-          onClick={() => {
-            localStorage.removeItem("token");
-          }}
-        >
-          <Link to="/login">Logout</Link>
-        </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
-};
