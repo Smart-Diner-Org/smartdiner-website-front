dd91005337a4e244726b5bc8cf44cc2749267eff
harith-beckham <harith.kv47@gmail.com>
2021-06-16 17:20:12 +0530

    added logo in both admin panel and smart diner super admin panel

--- src/Admin_Panel/SuperAdminPanel/Header.js
+++ src/Admin_Panel/SuperAdminPanel/Header.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { roles_and_IDs } from "../../helpers/constants";
+import Logo from "../assets/images/SmartDiner_logo.png";
 
 import Dropdown from "react-bootstrap/Dropdown";
 
 function Header(props) {
   const [show, setShow] = React.useState(false);
-  
+
   return (
     <>
       {!props.isMobile ? (
         <div className="admin-header">
           <div className="row d-flex">
             <div className="col-auto d-flex justify-content-around align-items-center">
-              <img src={props.logo} alt={`${props.name}`} />
-              <label className="ml-20 mb-0">{props.name}</label>
+              {props.logo ? (
+                <div>
+                  <a href="/">
+                    <span><img className="img-fluid" loading="lazy" src={props.logo} alt={`${props.name}`}></img></span>
+                  </a>
+                  <label className="ml-20 mb-0">{props.name}</label>
+                </div>
+              ) : (
+                  <>
+                    <img className="img-fluid" loading="lazy" src={Logo} alt={`${props.name}`}></img>
+                    <label className="ml-20 mb-0">{props.name}</label>
+                  </>
+                )
+              }
             </div>
             <div className="col-auto ml-auto d-flex align-items-center">
               <Dropdown>
@@ -40,7 +53,13 @@ function Header(props) {
                       </Link>
                     )}
                   </Dropdown.Item>
-
+                  <Dropdown.Item>
+                    {props.roleID === roles_and_IDs["Super Admin"] && (
+                      <Link to="/admin-panel/edit-menu">
+                        Edit Menu
+                </Link>
+                    )}
+                  </Dropdown.Item>
                   <Dropdown.Item
                     onClick={() => {
                       localStorage.removeItem("token");
@@ -84,7 +103,13 @@ function Header(props) {
                       </Link>
                     )}
                   </Dropdown.Item>
-
+                  <Dropdown.Item>
+                    {props.roleID === roles_and_IDs["Super Admin"] && (
+                      <Link to="/admin-panel/edit-menu">
+                        Edit Menu
+                </Link>
+                    )}
+                  </Dropdown.Item>
                   <Dropdown.Item
                     onClick={() => {
                       localStorage.removeItem("token");
