e6fd0ed0aea63892358d1e155cb1148e98558a6e
Nithish Kumar <nithishpadma@gmail.com>
2021-09-08 17:00:43 +0530

    remove harith,nandhini and add nithish profile

--- src/Landing_Page/Team/Team.js
+++ src/Landing_Page/Team/Team.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import MemberCard from "./MemberCard";
 import { useLocation, Link } from "react-router-dom";
 import ReactGA from "react-ga";
-import { anushree, dharee, divya, sandesh, sharmi, vinay, harith, karthik, nandhiniSiva, swathi } from "../s3images";
+import { anushree, dharee, divya, sandesh, sharmi, nithish, vinay, karthik, swathi } from "../s3images";
 
 function Team() {
   const { pathname } = useLocation();
@@ -51,18 +51,11 @@ function Team() {
                   twitter={"https://twitter.com/krajdsamy"}
                 />
                 <MemberCard
-                  name={"Harith Anand KV"}
+                  name={"Nithish Kumar"}
                   role={"Tech Intern"}
-                  photo={harith}
-                  linkedin={"https://www.linkedin.com/in/harith-anand/"}
-                  instagram={"https://www.instagram.com/harith_beckham_/"}
-                />
-                <MemberCard
-                  name={"Nandhini Siva"}
-                  role={"Tech Intern"}
-                  photo={nandhiniSiva}
-                  linkedin={"https://www.linkedin.com/in/nandhini-s-232000/"}
-                  twitter={"https://twitter.com/NandhiniSiva13"}
+                  photo={nithish}
+                  linkedin={"https://www.linkedin.com/in/nithish-kumar-1652ba18b/"}
+                  instagram={"https://www.instagram.com/nithish_170/"}
                 />
                 <MemberCard
                   name={"Swathi P"}
