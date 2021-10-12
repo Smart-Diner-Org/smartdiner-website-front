e15a2dd6764f6faa0fe74811392079d5040cbe29
Devesh Pandey <devesh_pandey@outlook.com>
2021-10-12 08:53:26 +0530

    added Devesh

---
+++ src/Landing_Page/Team/HelpingHands.js
@@ -0,0 +1,30 @@
+const HelpingHands=[ 
+    {
+    id: 1,
+    name:"Sandesh P S",
+    role:"Advisor, Technology",
+    photo:"https://smart-diner.s3.ap-south-1.amazonaws.com/team/sandesh.jpg",
+    linkedin:"https://www.linkedin.com/in/sandesh-ps-ab975041/",
+    twitter:"https://twitter.com/sandeshps_1989"
+    }
+    ,
+    {
+    id: 2,
+    name:"Karthikraj",
+    role:"Advisor, Technology",
+    photo:"https://smart-diner.s3.ap-south-1.amazonaws.com/team/karthikraj+2.jpg",
+    linkedin:"https://www.linkedin.com/in/karthikrajduraisamy/",
+    twitter:"https://twitter.com/krajdsamy"
+    }
+    ,
+    {
+    id: 3,
+    name:"Vinay Veerappaji",
+    role:"Software Engineer",
+    photo:"https://smart-diner.s3.ap-south-1.amazonaws.com/team/vinay.jpg",
+    linkedin:"https://www.linkedin.com/in/vinay-veerappaji/",
+    instagram:"https://www.instagram.com/vinay_veerappaji/"
+    }
+]
+
+export default HelpingHands;
---
+++ src/Landing_Page/Team/Members.js
@@ -0,0 +1,36 @@
+const Members=[ 
+{
+id: 1,
+name:"Sharmiladevi N S",
+role:"Founder",
+photo:"https://smart-diner.s3.ap-south-1.amazonaws.com/team/sharmi.jpg",
+linkedin:"https://www.linkedin.com/in/sharmiladevi-n-s-a03589a2/",
+twitter:"https://twitter.com/sharmiladevins"
+}
+,
+{
+id: 2,
+name:"Swathi P",
+role:"Sales Executive",
+photo:"https://smart-diner.s3.ap-south-1.amazonaws.com/team/swathi+profile.png",
+linkedin:"https://www.linkedin.com/in/swathi-p-103904187"
+}
+,
+{
+id: 3,
+name:"Devesh Pandey",
+role:"Software Engineer",
+photo:"https://smart-diner.s3.ap-south-1.amazonaws.com/team/devesh.png",
+linkedin:"https://www.linkedin.com/in/deveshpandey96/",
+twitter:"https://twitter.com/nameisdevesh"
+}
+,
+{
+id: 4,
+name:"Sangeetha",
+role:"Designer",
+photo:"https://smart-diner.s3.ap-south-1.amazonaws.com/team/sangeetha.jpg"
+}
+]
+
+export default Members;
